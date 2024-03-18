ce43057b02e40fa095d54edd1c0d63306ed5fb79
Andres Pinzon <pinnzonandres@gmail.com>
2024-03-18 18:50:18 -0500

    Mejora método SMOTE.ENC.FSDS

--- notebooks/R/Balanceo.R
+++ notebooks/R/Balanceo.R
@@ -1,139 +1,157 @@
+# Carga de Librerias
 library(FNN)
 library(modeest)
+library(dplyr)
+
+# Código Fuente con el método de generación de muestras de FSDS
 source("C:\\Users\\andre\\OneDrive\\Escritorio\\Proyecto de Grado\\notebooks\\R\\FSDS.R")
 
 
+
+
+
 # Función para encontrar los k vecinos más cercanos según la métrica dada por SMOTE.ENC
-SMOTE.ENC.KNN <- function(df, target, minority_value, k, seed){
-  # Plantación de Semilla
+SMOTE.ENC.KNN <- function(df, target, minority.value, vars.numeric, scaled = FALSE,  k, seed){
   set.seed(seed)
-  
-  # Detección de Variables Numéricas y Categóricas
-  vars.numeric <- names(df)[sapply(df, is.numeric)]
-  vars.categoric <- names(df)[sapply(df, is.factor)]
-  vars.categoric <- vars.categoric[vars.categoric != target] 
-  
-  # Escalamiendo de los datos
-  #df[vars.numeric] <- lapply(df[vars.numeric], scale)
-  # Simple Encoder para cálculo de distancias
+
+  # Detección de las variables categóricas según la lista numérica
+  vars.categoric <- names(df)[!names(df) %in% vars.numeric]
+  vars.categoric <- vars.categoric[vars.categoric != target]
+
+  # Escalamaiento de los Datos para el caso que sea requerido
+  if (scaled){
+    df[vars.numeric] <- lapply(df[vars.numeric], scale)
+  }
+
+  # Mutación de los conjuntos para asegurar que las variables sean factores y valores numéricos
+  df = df %>% mutate(across(all_of(vars.categoric), factor))
+  #df = df %>% mutate(across(all_of(vars.numeric), numeric))
+
+
+  # Ajuste del conjunto de la clase minoritaría
+  minority.df = df[df[[target]] == minority.value, ]
+
+  # Creación de conjuntos Codificados para las variables categóricas
   encoded.categoric = as.data.frame(sapply(df[vars.categoric], as.numeric))
-  
-  # Detección del Conjunto Minoritario
-  minor_df = df[df[[target]] == minority_value, ]
-  
-  encoded.minority = encoded.categoric[as.integer(rownames(minor_df)),]
+  encoded.minority = encoded.categoric[as.integer(rownames(minority.df)),]
+
   # Variables Necesarias para el cálculo de la distancia para SMOTE ENC
-  t = nrow(minor_df)
+  t = nrow(minority.df)
   s = nrow(df)
-  
   ir = t/s
   c = length(vars.numeric)
-  m = ifelse(c>0, median(sapply(minor_df[vars.numeric], sd)), 1)
-  
-  # Función que determina el vector de valores determinado a cada valor de cada variable
-  SMOTE.categoric.distance <- function(data, categoric, m){
-    
-    l_values <- vector(mode = "list", length = length(categoric))
-    names(l_values) <- categoric
-    
-    # Calcular 'l' para cada variable categórica en una sola línea
-    for (var in categoric){
-      labels <- unique(data[, var])
-      e <- sapply(labels, function(l) sum(data[, var] == l))
-      e_star <- e * ir
-      o <- sapply(labels, function(l) sum(encoded.minority[, var] == l))
-      chi <- (o - e_star)/(e_star)
-      l <- chi*m
-      l[labels]
-      l_values[[var]] = l
-    }
-    
-    return(l_values)
-  }
-  # Cálculo de los valores
-  values.categoric <- SMOTE.categoric.distance(encoded.categoric, vars.categoric, m)
-  
-  # Crear una copia de encoded.categoric para mantener el original intacto
-  encoded.categoric_updated <- encoded.categoric
-  
-  # Aplicar la transformación a través de todas las variables categóricas
-  encoded.categoric_updated[vars.categoric] <- lapply(names(encoded.categoric[vars.categoric]), function(var) {
-    lapply(encoded.categoric[[var]], function(value) {
-      values.categoric[[var]][value]
-    })
+  m = ifelse(c>0, median(sapply(minority.df[vars.numeric], sd)), 1)
+
+  # Cálculo del Valor Asociado a cada nivel de cada variable categórica según SMOTE ENC
+  l_values <- lapply(vars.categoric, function(var){
+    labels <- unique(encoded.categoric[, var])
+    e <- sapply(labels, function(l) sum(encoded.categoric[, var] == l))
+    e_star <- e * ir
+    o <- sapply(labels, function(l) sum(encoded.minority[, var] == l))
+    chi <- (o - e_star)/(e_star)
+    l <- chi*m
+    l[labels]
   })
+  names(l_values) <- vars.categoric
+
+  # Creación del dataframe donde se almacenará la información numérica de los valores asociados a las variables categóricas
+  encoded.categoric_updated <- encoded.categoric
+
+  encoded.categoric_updated[vars.categoric] <- lapply(vars.categoric, function(var) {
+  lapply(encoded.categoric[[var]], function(value) {
+    l_values[[var]][value]
+  })
+})
+
+  # Concatenación del resultado de las variables categóricas con las variable numéricas
   df_encoded <- cbind(df[vars.numeric], encoded.categoric_updated)
-  df_encoded = data.frame(df_encoded)
-  
-  df_encoded_numeric <- apply(df_encoded, 2, as.numeric)
-  
+  df_encoded = data.frame(df_encoded) 
+  df_encoded_numeric <- mutate_all(df_encoded, as.numeric)
+
+  # Detección de los k vecinos más cercanos
   knn = get.knn(df_encoded_numeric, k)$nn.index
-  
+
   return(knn)
 }
 
-generacion_muestra <- function(df, target, minority.value, str_weight, k, vars.categoric, vars.numeric, seed){
-    # Plantación de Semilla
-    set.seed(seed)
 
-    # Detección del conjunto minoritario
-    minority.df = df[df[[target]] == minority.value, ]
-    
-    # Definición de valores necesarios
-    S = nrow(df)
-    minority.less = S - 2*nrow(minority.df)
+SyntheticData <- function(df, vars.numeric, target, minority.value, minority.less, muestra, knn, str_weight_name, seed){
+  # Plantación de Semilla
+  set.seed(seed)
 
-    m = 1
-    n = S + minority.less
-    N = round(sum(df[[str_weight]]),0)
+  # Detección de variables categóricas
 
-    # Ajuste de Pesos
-    ratio.ajuste = S/n
-    df[['Adjusted_Weight']] <- df[[str_weight]]*ratio.ajuste
+  vars.categoric <- names(df)[!names(df) %in% vars.numeric]
+  vars.categoric <- vars.categoric[vars.categoric != target]
 
-    factor.expansion <- df[['Adjusted_Weight']]
-
-    D_m = FSDS(minority.df, factor.expansion, N, m, n, seed = seed)
-
-    muestra = D_m[[1]][sample(1:n, minority.less),]
-    rownames(muestra) <- NULL
-
-    subset.df = subset(df, select = colnames(df)[colnames(df) != str_weight])
-    
-    # Obtención de los k vecinos más cercanos según la metrica SMOTE ENC
-    knn = SMOTE.ENC.KNN(subset.df, target, minority.value ,k, seed)
-
-    # Generación de las muestras sintéticas
-    Synthetic <- data.frame(matrix(nrow = minority.less, ncol = ncol(df)))
-    colnames(Synthetic) <- colnames(df)
-    Synthetic[vars.categoric] = as.factor(Synthetic[vars.categoric])
-    
-    # Indice que permite recorrer cada registro de la matriz sinteética
-    Synthetic.index = 0
-    
-    for (j in 1:minority.less){
-    id_muestra = muestra[j, 'new_indice']
-    i = as.integer(rownames(minority.df[id_muestra,]))
-        
-    k_neighbors = knn[i,]
-    
-    nn <- sample(k_neighbors,1)
-    
-    for (var in vars.numeric){
-        diff = df[[var]][nn] - df[[var]][i]
-        gap <- runif(1)
-        
-        Synthetic[[var]][Synthetic.index +1] <- df[[var]][i] + gap*diff
-    }
-    for (var in vars.categoric){
-        value = as.character(mfv(df[[var]][k_neighbors])[1])
-        Synthetic[[var]][Synthetic.index+1] = value
-    }
-    Synthetic[[target]] = minority.value
-    Synthetic[['Adjusted_Weight']] = muestra[['new_weight']]
-    Synthetic.index = Synthetic.index + 1
+  a = sapply(1:minority.less, function(j){
+    id_muestra = muestra[j,'indice']
+    k_nn = knn[id_muestra,]
+    nn <- sample(k_nn,1)
+    numeric.values = sapply(vars.numeric, function(var){
+      diff = df[[var]][nn] - df[[var]][id_muestra]
+      gap <- runif(1)
+      return(df[[var]][id_muestra] + gap*diff)
+    })
+    names(numeric.values) = vars.numeric
+  
+    categoric.values = sapply(vars.categoric, function(var){
+      value = as.character(mfv(df[[var]][k_nn])[1])
+      return(value)
+    })
+    names(categoric.values) = vars.categoric
+  
+    synthetic = c(numeric.values, categoric.values)
+    return(synthetic)
+  })
+  
+  result = data.frame(t(a))
+  result[[str_weight_name]] = muestra[, 'weight']
+  result[[target]] = minority.value
+  return(result)
 }
 
-resample.data = rbind(df, Synthetic)
-return(resample.data)
+SMOTE.ENC.FSDS <- function(df, target, minority.value, str_weight_name, k, vars.numeric, seed){
+  # Plantación de Semilla
+  set.seed(seed)
+  vars.numeric <- vars.numeric[vars.numeric != str_weight_name]
+  
+  # Detección del conjunto minoritario
+  minority.df = df[df[[target]] == minority.value, ]
+  
+  # Definición de valores necesarios
+  S = nrow(df)
+  minority.less = S - 2*nrow(minority.df)
+  
+  m = 1
+  n = S + minority.less
+  N = round(sum(df[[str_weight_name]]),0)
+  
+  # Ajuste de Pesos
+  ratio.ajuste = S/n
+  df[['Adjusted_Weight']] <- df[[str_weight_name]]*ratio.ajuste
+  
+  factor.expansion <- df[['Adjusted_Weight']]
+  
+  # Generación de Muestra
+  print("Making Muestra")
+  D_m = FSDS(minority.df, factor.expansion, N, m, n, seed)
+  
+  muestra = D_m[[1]][sample(1:n, minority.less),]
+  rownames(muestra) <- NULL
+  
+  # Detección de K vecinos
+  print("Hallando KNN")
+  subset.df = subset(df, select = colnames(df)[colnames(df) != str_weight_name])
+  knn = SMOTE.ENC.KNN(subset.df, target, minority.value, vars.numeric, FALSE, k, seed)
+  
+  # Generación Muestra Sintética
+  print("Generando Synthetic")
+  Synthetic = SyntheticData(df, vars.numeric, target, minority.value, minority.less, muestra, knn, 'Adjusted_Weight', 3)
+  
+  
+  # Concatenación de Resultados
+  resampled_data = rbind(df, Synthetic)
+  
+  return(resampled_data)
 }
--- notebooks/R/FSDS.R
+++ notebooks/R/FSDS.R
@@ -3,43 +3,50 @@
 library(tidyverse)
 
 ## Función de cálculo
-fsds_DM <- function(indice, factor_expansion, N, n, restante, seed) {
+fsds_sample <- function(indice, factor_expansion, N, n, seed){
+  restante = N-n
+  limite = ifelse(restante>50*n, 50*n, restante)
   set.seed(seed)
-  # Paso 1: Extracción de muestras y cálculo del nuevo peso
+  
   freq <- table(indice)
   sumatoria <- sum(freq[as.character(indice)] * factor_expansion[indice])
   pesos_ajustados <- (N* factor_expansion[indice]*freq[as.character(indice)])/sumatoria
   
-  # Paso 2: Selección de datos con probabilidad ajustada
-  new_indice <- indice
-  new_weight = pesos_ajustados
-  for (i in 1:restante){
-    draw = sample(indice, size = 1, replace = TRUE, prob = pesos_ajustados)
-    p_i = (pesos_ajustados[as.character(draw)] - 1 + 
-             (table(new_indice)[as.character(draw)]*(restante)/n))/(N-n + ((i -1)*(restante)/n))
-    new_weight = c(new_weight, p_i)
-    new_indice = c(new_indice, draw)
-  }
+  pesos_ajustados
   
-  # Paso 3: Creación de la pseudo-población y extración de la muestra final
-  pseudo_m <- data.frame(cbind(new_indice, new_weight))
-  rownames(pseudo_m) <- NULL
-  D_m <- pseudo_m[sample(1:n),]
+  draw <- sample(indice, size = limite, replace = TRUE, prob = pesos_ajustados)
+  new_indice = c(indice, draw)
+  print("Pseudo Poblation")
+  p_i = unlist(lapply((n+1):(limite+n), function(i){
+    new_freq = table(new_indice[1:i])
+    numerador = pesos_ajustados[as.character(new_indice[i])]-1 +
+     (new_freq[as.character(new_indice[i])])*(restante/(n))
+    denominador = restante + ((i-n)-1)*restante/n
+    p_i = (denominador/numerador)
+    return(p_i)
+  }))
   
-  # Regresar la muestra final
+  new_weight = c(pesos_ajustados, p_i)
+  
+  pseudo = data.frame(
+    indice = new_indice,
+    weight = new_weight
+  )
+  
+  D_m <- pseudo[sample(1:nrow(pseudo),n),]
+
   return(D_m)
 }
 
 FSDS <- function(df, factor_expansion, N, m, n, seed){
   set.seed(seed)
   indices <- lapply(1:m, function(i) {
-    sample(1:nrow(df), size = n, replace = TRUE)
-  })
-  restante = N - m
-  
+  sample(as.numeric(rownames(df)), size = n, replace = TRUE)
+})
+
   D_m <- lapply(1:m, function(i) {
-    fsds_DM(indices[[i]], factor_expansion, N, n, restante, seed)
-  })
+  fsds_sample(indices[[i]], factor_expansion, N, n, seed)
+})
   
   return(D_m)
 }
